75313d7e447161d8e637daafdb984a1e71d887db
Thomas Lee <thomasslee97@gmail.com>
2018-02-11 16:56:20 +0000

    Commented  getStateFromJSON()

--- core/src/sepr/game/saveandload/JSONifier.java
+++ core/src/sepr/game/saveandload/JSONifier.java
@@ -12,103 +12,114 @@ import sepr.game.utils.TurnPhaseType;
 
 import java.util.*;
 
+/**
+ * Class to convert the game state to and from a JSON representation
+ */
 public class JSONifier {
 
-    public GameState state;
-    public JSONObject saveState;
+    public GameState state; // The state of the game
+    public JSONObject saveState; // The JSON state of the game
 
-    public JSONifier(){
-
-    }
-
+    /**
+     * Set the state to represent as JSON
+     * @param state The game state
+     */
     public void SetState(GameState state){
         this.state = state;
     }
 
+    /**
+     * Set the JSON Object to read from
+     * @param json JSON Representation of game state to load
+     */
     public void SetStateJSON(JSONObject json){
         this.saveState = json;
     }
 
+    /**
+     * Get a game state from its JSON representation
+     * @return GameState to load
+     */
     public GameState getStateFromJSON() {
-        GameState gameState = new GameState();
-        gameState.currentPhase = this.StringToPhase(this.saveState.get("CurrentPhase").toString());
+        GameState gameState = new GameState(); // GameState to return
+        gameState.currentPhase = this.StringToPhase(this.saveState.get("CurrentPhase").toString()); // Get the current stage
 
-        JSONArray sectors = (JSONArray) this.saveState.get("MapState");
-        gameState.mapState = gameState.new MapState();
-        gameState.mapState.sectorStates = new GameState.SectorState[sectors.size()];
+        JSONArray sectors = (JSONArray) this.saveState.get("MapState"); // Get a JSONArray of the map Sectors
+        gameState.mapState = gameState.new MapState(); // MapState to return
+        gameState.mapState.sectorStates = new GameState.SectorState[sectors.size()]; // Array of SectorStates to load to the map
 
-        int i = 0;
+        int i = 0; // Index of current sector state to load
 
-        for (Object obj : sectors){
-            JSONObject sector = (JSONObject)obj;
+        for (Object obj : sectors){ // Iterate through JSON sectors
+            JSONObject sector = (JSONObject)obj; // Cast to JSONObject
 
-            gameState.mapState.sectorStates[i] = gameState.new SectorState();
+            gameState.mapState.sectorStates[i] = gameState.new SectorState(); // SectorState to load
 
-            gameState.mapState.sectorStates[i].hashMapPosition = (int)(long)(Long)sector.get("HashMapPosition");
-            gameState.mapState.sectorStates[i].id = (int)(long)(Long)sector.get("ID");
-            gameState.mapState.sectorStates[i].ownerId = (int)(long)(Long)sector.get("OwnerID");
-            gameState.mapState.sectorStates[i].displayName = (String)sector.get("DisplayName");
-            gameState.mapState.sectorStates[i].unitsInSector = (int)(long)(Long)sector.get("UnitsInSector");
-            gameState.mapState.sectorStates[i].reinforcementsProvided = (int)(long)(Long)sector.get("ReinforcementsProvided");
-            gameState.mapState.sectorStates[i].college = (String)sector.get("College");
-            gameState.mapState.sectorStates[i].texturePath = (String)sector.get("TexturePath");
-            gameState.mapState.sectorStates[i].neutral = (Boolean)sector.get("Neutral");
+            gameState.mapState.sectorStates[i].hashMapPosition = (int)(long)(Long)sector.get("HashMapPosition"); // Get Sector's HashMap position
+            gameState.mapState.sectorStates[i].id = (int)(long)(Long)sector.get("ID"); // Get Sector's ID
+            gameState.mapState.sectorStates[i].ownerId = (int)(long)(Long)sector.get("OwnerID"); // Get Sector's Owner's ID
+            gameState.mapState.sectorStates[i].displayName = (String)sector.get("DisplayName"); // Get Sector's display name
+            gameState.mapState.sectorStates[i].unitsInSector = (int)(long)(Long)sector.get("UnitsInSector"); // Get the number of units in the Sector
+            gameState.mapState.sectorStates[i].reinforcementsProvided = (int)(long)(Long)sector.get("ReinforcementsProvided"); // Get the number of reinforcements provided
+            gameState.mapState.sectorStates[i].college = (String)sector.get("College"); // Get the Sector's college
+            gameState.mapState.sectorStates[i].texturePath = (String)sector.get("TexturePath"); // Get the Sector's texture filepath
+            gameState.mapState.sectorStates[i].neutral = (Boolean)sector.get("Neutral"); // Get whether the Sector is nneutral
 
-            JSONArray adjacentSectors = (JSONArray)sector.get("AdjacentSectorIDs");
-            gameState.mapState.sectorStates[i].adjacentSectorIds = new int[adjacentSectors.size()];
+            JSONArray adjacentSectors = (JSONArray)sector.get("AdjacentSectorIDs"); // Get the JSONArray of adjacent Sectors
+            gameState.mapState.sectorStates[i].adjacentSectorIds = new int[adjacentSectors.size()]; // Create a new array of adjacent Sectors
 
-            int j = 0;
+            int j = 0; // Index of current adjacent sector
 
-            for (Object adj : adjacentSectors){
+            for (Object adj : adjacentSectors){ // Iterate through adjacent sectors and add each to the adjacent sectors
                 gameState.mapState.sectorStates[i].adjacentSectorIds[j] = (int)(long)(Long)adj;
 
                 j++;
             }
 
-            gameState.mapState.sectorStates[i].sectorCentreX = (int)(long)(Long)sector.get("SectorCenterX");
-            gameState.mapState.sectorStates[i].sectorCentreY = (int)(long)(Long)sector.get("SectorCenterY");
-            gameState.mapState.sectorStates[i].decor = (Boolean)sector.get("Decor");
-            gameState.mapState.sectorStates[i].fileName = (String)sector.get("FileName");
-            gameState.mapState.sectorStates[i].allocated = (Boolean)sector.get("Allocated");
+            gameState.mapState.sectorStates[i].sectorCentreX = (int)(long)(Long)sector.get("SectorCenterX"); // Get the Sector's X coordinate
+            gameState.mapState.sectorStates[i].sectorCentreY = (int)(long)(Long)sector.get("SectorCenterY"); // Get the Sector's Y coordinate
+            gameState.mapState.sectorStates[i].decor = (Boolean)sector.get("Decor"); // Get whether the Sector is decor
+            gameState.mapState.sectorStates[i].fileName = (String)sector.get("FileName"); // Get the file name of the Sector
+            gameState.mapState.sectorStates[i].allocated = (Boolean)sector.get("Allocated"); // Get whether the sector has been allocated
 
             i++;
         }
 
-        JSONArray players = (JSONArray)this.saveState.get("PlayerState");
-        gameState.playerStates = new GameState.PlayerState[players.size()];
+        JSONArray players = (JSONArray)this.saveState.get("PlayerState"); // Get the JSONArray of player states
+        gameState.playerStates = new GameState.PlayerState[players.size()]; // Create an array of Player States
 
-        int k = 0;
+        int k = 0; // Current index of player states
 
-        for (Object pl : players){
+        for (Object pl : players){ // Iterate through players
             JSONObject player = (JSONObject)pl;
 
-            gameState.playerStates[k] = gameState.new PlayerState();
+            gameState.playerStates[k] = gameState.new PlayerState(); // Create new PlayerState
 
-            gameState.playerStates[k].hashMapPosition = (int)(long)(Long)player.get("HashMapPosition");
-            gameState.playerStates[k].id = (int)(long)(Long)player.get("ID");
-            gameState.playerStates[k].collegeName = GameSetupScreen.CollegeName.fromString((String)player.get("CollegeName"));
-            gameState.playerStates[k].playerName = (String)player.get("PlayerName");
-            gameState.playerStates[k].troopsToAllocate = (int)(long)(Long)player.get("TroopsToAllocate");
-            JSONObject colour = (JSONObject)player.get("SectorColour");
-            gameState.playerStates[k].sectorColour = new Color((float)(double)(Double)colour.get("R"),(float)(double)(Double)colour.get("G"),(float)(double)(Double)colour.get("B"),(float)(double)(Double)colour.get("A"));
-            gameState.playerStates[k].playerType = PlayerType.fromString((String)player.get("PlayerType"));
-//            gameState.playerStates[k].ownsPVC = (boolean)(Boolean)player.get("OwnsPVC");
+            gameState.playerStates[k].hashMapPosition = (int)(long)(Long)player.get("HashMapPosition"); // Get Player's HashMap position
+            gameState.playerStates[k].id = (int)(long)(Long)player.get("ID"); // Get Player's ID
+            gameState.playerStates[k].collegeName = GameSetupScreen.CollegeName.fromString((String)player.get("CollegeName")); // Get Player's college name
+            gameState.playerStates[k].playerName = (String)player.get("PlayerName"); // Get Player's name
+            gameState.playerStates[k].troopsToAllocate = (int)(long)(Long)player.get("TroopsToAllocate"); // Get the troops that the Player has left to allocate
+            JSONObject colour = (JSONObject)player.get("SectorColour"); // Get the Player's Sector colour
+            gameState.playerStates[k].sectorColour = new Color((float)(double)(Double)colour.get("R"),(float)(double)(Double)colour.get("G"),(float)(double)(Double)colour.get("B"),(float)(double)(Double)colour.get("A")); // Read Sector colour into a new colour object
+            gameState.playerStates[k].playerType = PlayerType.fromString((String)player.get("PlayerType")); // Get the Player's type
+            gameState.playerStates[k].ownsPVC = (boolean)(Boolean)player.get("OwnsPVC"); // Get whether the Player owns the PVC
 
             k++;
         }
 
-        gameState.turnTimerEnabled = (Boolean)this.saveState.get("TurnTimerEnabled");
-        gameState.maxTurnTime = (int)(long)(Long)this.saveState.get("MaxTurnTime");
-        gameState.turnTimeStart = (Long)this.saveState.get("TurnTimeStart");
+        gameState.turnTimerEnabled = (Boolean)this.saveState.get("TurnTimerEnabled"); // Get whether the turn timer is enabled
+        gameState.maxTurnTime = (int)(long)(Long)this.saveState.get("MaxTurnTime"); // Get the maximum turn time
+        gameState.turnTimeStart = (Long)this.saveState.get("TurnTimeStart"); // Get the start time of the turn
 
-        gameState.turnOrder = new ArrayList<Integer>();
-        JSONArray turnOrderJSON = (JSONArray)this.saveState.get("TurnOrder");
+        gameState.turnOrder = new ArrayList<Integer>(); // Turn order
+        JSONArray turnOrderJSON = (JSONArray)this.saveState.get("TurnOrder"); // Get the turn order JSONArray
 
-        for (Object obj : turnOrderJSON){
+        for (Object obj : turnOrderJSON){ // Iterate through the turn order array and add the order to the game state
             gameState.turnOrder.add((int)(long)(Long)obj);
         }
 
-        gameState.currentPlayerPointer = (int)(long)(Long)this.saveState.get("CurrentPlayerPointer");
+        gameState.currentPlayerPointer = (int)(long)(Long)this.saveState.get("CurrentPlayerPointer"); // Get the pointer to the current Player
 
         return gameState;
     }
