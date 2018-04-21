a8da4736fbde34b5fe35b6e32df88ae6f9f9b502
sdp518 <sdp518@york.ac.uk>
2018-04-22 00:33:55 +0100

    Adding loading widget

--- core/src/sepr/game/GameSetupScreen.java
+++ core/src/sepr/game/GameSetupScreen.java
@@ -52,6 +52,11 @@ public class GameSetupScreen implements Screen{
 
     private Texture collegeTableBackground;
 
+    // NEW ASSESSMENT 4
+    private Stage loadingWidgetStage;
+    private boolean isLoading;
+    private boolean loadingWidgetDrawn;
+
     /**
      * the colleges available to play as
      */
@@ -116,6 +121,11 @@ public class GameSetupScreen implements Screen{
 
         this.collegeTableBackground = new Texture("uiComponents/Game-Setup-Name-Box.png");
         this.Audio.loadMusic("sound/IntroMusic/introMusic.mp3"); //load the introMusic
+
+        this.loadingWidgetStage = new Stage();
+        this.isLoading = false;
+        this.loadingWidgetDrawn = false;
+
         this.setupUi();
     }
 
@@ -462,7 +472,7 @@ public class GameSetupScreen implements Screen{
     }
 
     /**
-     * Method attempts to start the game
+     * Method checks if the given settings are valid
      * Following conditions must be met for a game to be able to start
      *  At least 2 players
      *  At least 1 human player
@@ -472,7 +482,7 @@ public class GameSetupScreen implements Screen{
      *  Player names must be at least three characters long
      *  Player names contain numbers and digits only
      */
-    private void startGame() {
+    private void validateGame() {
         try {
             validatePlayerNames();
             validateCollegeSelection();
@@ -481,9 +491,16 @@ public class GameSetupScreen implements Screen{
             DialogFactory.basicDialogBox("Game Setup Error", e.getExceptionType().getErrorMessage(), stage);
             return;
         }
-        HashMap<Integer, Player> x = generatePlayerHashmaps();
+        Gdx.input.setInputProcessor(null); //Blocks user input as the game is now loading
+        this.showLoadingWidget();
+    }
 
-        int MAX_TURN_TIME = 120;
+    /**
+     * Method starts the game
+     */
+    private void startGame() {
+        HashMap<Integer, Player> x = generatePlayerHashmaps();
+        int MAX_TURN_TIME = 300;
         Audio.disposeMusic("sound/IntroMusic/introMusic.mp3");
         Audio.loadMusic("sound/Gameplay Music/wind.mp3"); //loads and plays the gamePlay music
         main.setGameScreen(x, turnTimerSwitch.isChecked(), MAX_TURN_TIME, neutralPlayerSwitch.isChecked());
@@ -497,7 +514,7 @@ public class GameSetupScreen implements Screen{
         startGameButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                startGame();
+                validateGame();
             }
         });
 
@@ -529,6 +546,19 @@ public class GameSetupScreen implements Screen{
         })).colspan(2);
     }
 
+    /**
+     * sets up loading widget to be shown when game starts
+     */
+    @SuppressWarnings("Duplicates")
+    private void showLoadingWidget() {
+        isLoading = true;
+        Table table = new Table();
+        table.setDebug(false);
+        table.setFillParent(true);
+        table.add(new Image(new Texture("uiComponents/loadingBox.png")));
+        loadingWidgetStage.addActor(table);
+    }
+
 
     /**
      * change the input processing to be handled by this screen's stage
@@ -540,9 +570,16 @@ public class GameSetupScreen implements Screen{
 
     @Override
     public void render(float delta) {
+        if (loadingWidgetDrawn) {
+            startGame();
+        }
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         this.stage.act(Gdx.graphics.getDeltaTime());
         this.stage.draw();
+        if (isLoading) {
+            loadingWidgetStage.draw();
+            loadingWidgetDrawn = true;
+        }
     }
 
     @Override
